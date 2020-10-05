ced676292e82836a0700eb86e451687d2f853716
VladyslavPrytula <vladyslavprytula.dev@gmail.com>
2020-10-05 15:51:59 +0300

    added hw without styles

--- src/Components/ContactsList/ContactList.js
+++ src/Components/ContactsList/ContactList.js
@@ -1,7 +1,5 @@
 import React from "react";
 import PropTypes from "prop-types";
-// import classNames from "classnames";
-// import "./TodoList.scss";
 
 const ContactList = ({ contacts, onDeleteContact }) => (
   <>
--- src/Components/Filter/Filter.js
+++ src/Components/Filter/Filter.js
@@ -1,17 +1,15 @@
 import React from "react";
 import PropTypes from "prop-types";
-// import './Filter.scss';
 
 const Filter = ({ value, onChangeFilter }) => {
   return (
-    <label className="Filter__label">
+    <label>
       Find contacts by name
       <br />
       <input
         type="text"
         value={value}
         onChange={(e) => onChangeFilter(e.target.value)}
-        className="Filter__input"
         placeholder="Type name... "
       />
     </label>
